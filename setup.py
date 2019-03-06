234f0eedb0742920cdf26da9bc84bf3f863a2f02
Bobby Norton <bobby@testedminds.com>
2019-03-06 15:52:13 -0600

    Create a new Docker release

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ try:
 except ImportError:
     long_description = open('README.md').read()
 
-from sand import __version__
+# Read version programmatically to avoid loading dependencies from sand/__init__.py
+exec(open('sand/__version__.py').read())
 
 setup(
     name='sand',
@@ -36,10 +37,7 @@ setup(
         'License :: OSI Approved :: Apache Software License',
 
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3 :: Only'
     ],
     keywords=['Architecture', 'Network', 'Graph'],
 
