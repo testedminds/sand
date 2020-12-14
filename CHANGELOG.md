ddc2368cdcb792bd1858a295a9a9b43ac0cadfef
Bobby Norton <bobby@testedminds.com>
2020-12-14 09:24:29 -0600

    Bump version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,14 @@ _`sand` adheres to [Semantic Versioning](http://semver.org)._
 
 __This file is organized according to the conventions in [Keep a CHANGELOG](http://keepachangelog.com)._
 
+## [0.2.1] - 2020-12-14
+
+- Upgrade version ranges in dependencies.
+
+## [0.2.0] - 2019-03-06
+
+- Python3 support
+
 ## [0.1.2] - 2017-08-25
 
 - Fix issue with missing packages in distribution.
--- setup.py
+++ setup.py
@@ -37,7 +37,13 @@ setup(
         'License :: OSI Approved :: Apache Software License',
 
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3 :: Only'
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     keywords=['Architecture', 'Network', 'Graph'],
 
